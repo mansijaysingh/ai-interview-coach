c3ac8d7d42f42d66b9d10765b3014014634ed75e
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-24 14:37:47 +0530

    Added role-based interview question generation

--- app.py
+++ app.py
@@ -7,18 +7,47 @@ load_dotenv()
 
 client=OpenAI()
 
-def ask_question(question):
+role=input("Enter your role (Python / AI / Frontend): ")
+difficulty=input("Enter difficulty level (Easy / Medium / Hard): ")
+
+# def ask_question(question):
+#   response=client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#       {"role":"user", "content": question}
+#     ]
+#   )
+#   return response.choices[0].message.content
+
+def generate_question(role, difficulty):
+  prompt=f"""
+    You are an expert technical interviewer.
+
+    Generate one {difficulty} level interview question for a {role} role.
+
+    Only give the question, no explanation.
+    """
   response=client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4o-mini",
     messages=[
-      {"role":"user", "content": question}
+      {"role":"user", "content":prompt}
     ]
   )
+
   return response.choices[0].message.content
 
-if __name__ =="__main__":
-  user_input=input("Enter your interview question: ")
-  answer=ask_question(user_input)
-  print("\nAI Response:\n")
-  print(answer)
-  
+
+question=generate_question(role,difficulty)
+print(question)
+answer = input("\nYour Answer: ")
+
+
+
+
+
+
+# if __name__ =="__main__":
+#   user_input=input("Enter your interview question: ")
+#   answer=ask_question(user_input)
+#   print("\nAI Response:\n")
+#   print(answer)
