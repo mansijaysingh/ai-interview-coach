7dceae6d02859fad5a76c3dfc46683789966c52a
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-25 16:15:39 +0530

    Step 2: Added interview setup form with inputs

--- UI.py
+++ UI.py
@@ -6,3 +6,30 @@ st.title("🤖 AI Interview Coach")
 st.subheader("Practice. Evaluate. Improve.")
 
 st.write("Welcome! Start your AI-powered interview practice.")
+
+st.markdown("👉Setup Your Interview")
+
+# Role selection
+roles = [
+    "Python",
+    "AI/ML",
+    "Frontend",
+    "Backend",
+    "Full Stack",
+    "Data Analyst",
+    "HR"
+]
+
+role=st.selectbox("Select Role", roles)
+
+# Experience
+experience = st.selectbox("Experience Level", ["Junior", "Mid", "Senior"])
+
+# Difficulty
+difficulty = st.selectbox("Difficulty", ["Easy", "Medium", "Hard"])
+
+# Number of questions
+num_questions = st.slider("Number of Questions", 1, 30, 5)
+
+# Start button
+start = st.button("Start Interview 🚀")
